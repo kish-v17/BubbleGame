0fe91416a7324c426f358bd831cd3626cd88454f
Kish-v17 <kish.v07@gmail.com>
2024-09-14 12:37:39 +0530

    responsive

--- script.js
+++ script.js
@@ -5,19 +5,14 @@ function makeBubbles(){
     var bubbles = "";
     var bottomDiv = document.querySelector(".bottom");
 
-    // Get the width and height of the bottom div
     var bottomWidth = bottomDiv.offsetWidth;
     var bottomHeight = bottomDiv.offsetHeight;
+    var bubbleSize = 40; 
+    var gapSize = 5;     
 
-    // Define the bubble size and gap
-    var bubbleSize = 40; // Bubble size in pixels
-    var gapSize = 5;     // Gap between bubbles
-
-    // Calculate how many bubbles can fit in one row and how many rows can fit
     var bubblesPerRow = Math.floor(bottomWidth / (bubbleSize + gapSize));
     var totalRows = Math.floor(bottomHeight / (bubbleSize + gapSize));
 
-    // Calculate the total number of bubbles that fit in the available space
     var totalBubbles = bubblesPerRow * totalRows;
 
     for (i = 1; i <= totalBubbles; i++) {
@@ -25,7 +20,6 @@ function makeBubbles(){
         bubbles += `<div class="bubble">${r}</div>`;
     }
 
-    // Update the .bottom div with the calculated number of bubbles
     bottomDiv.innerHTML = bubbles;
 }
 
