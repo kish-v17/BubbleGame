61395850dd45698106f4cc52b6160deacee1eb15
Kish-v17 <kish.v07@gmail.com>
2024-09-14 12:31:47 +0530

    responsive

--- script.js
+++ script.js
@@ -2,12 +2,31 @@ var time=60;
 var hit=0;
 var score=0;
 function makeBubbles(){
-var bubbles="";
-for(i=1;i<=175;i++){
-    var r=Math.floor(Math.random()*10);
-    bubbles+=`<div class="bubble">${r}</div>`;
-}
-document.querySelector(".bottom").innerHTML=bubbles;
+    var bubbles = "";
+    var bottomDiv = document.querySelector(".bottom");
+
+    // Get the width and height of the bottom div
+    var bottomWidth = bottomDiv.offsetWidth;
+    var bottomHeight = bottomDiv.offsetHeight;
+
+    // Define the bubble size and gap
+    var bubbleSize = 40; // Bubble size in pixels
+    var gapSize = 5;     // Gap between bubbles
+
+    // Calculate how many bubbles can fit in one row and how many rows can fit
+    var bubblesPerRow = Math.floor(bottomWidth / (bubbleSize + gapSize));
+    var totalRows = Math.floor(bottomHeight / (bubbleSize + gapSize));
+
+    // Calculate the total number of bubbles that fit in the available space
+    var totalBubbles = bubblesPerRow * totalRows;
+
+    for (i = 1; i <= totalBubbles; i++) {
+        var r = Math.floor(Math.random() * 10);
+        bubbles += `<div class="bubble">${r}</div>`;
+    }
+
+    // Update the .bottom div with the calculated number of bubbles
+    bottomDiv.innerHTML = bubbles;
 }
 
 function runTimer(){
